fd1b1b6e1ba1088abd23faedc6478bc3d85f4d15
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-08-09 22:03:59 +0200

    [types] - issue #2 Type exercice

---
+++ types.ts
@@ -0,0 +1,11 @@
+export interface Exercice {
+    id? : string,
+    nom? : string,
+    nbSeries? : number,
+    nbRepetitions? : number,
+    intensite? : number,
+    charge? : number,
+    tempsRepos? : number,
+    tempos? : string,
+    sensation? : string
+}
