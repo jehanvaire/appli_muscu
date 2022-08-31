3bdd53dd4c1c02342c65bb6699a2994e8eddd709
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-08-31 21:23:15 +0200

    [runSeance] - Chrono séance

--- components/exerciceCard.tsx
+++ components/exerciceCard.tsx
@@ -1,78 +1,152 @@
-import React, {useState} from 'react';
-import {Text, View, StyleSheet, Pressable} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Pressable, Button } from 'react-native';
 
-const ExerciceCard = (props : any) => {
+const ExerciceCard = (props: any) => {
+  const [exercice, setExercice] = useState(props.exercice);
+  const [currentSerie, setCurrentSerie] = useState(0);
+  const [info, setInfo] = useState('');
+  const [isBreak, setIsBreak] = useState(false);
+  const [currentBreakTime, setIsCurrentBreakTime] = useState(exercice.tempsRepos);
+  const [currentTime, setCurrentTime] = useState(currentBreakTime);
 
-    const [exercice, setExercice] = useState(props.exercice);
-    const [runExercice, setRunExercice] = useState(false);
-    const [currentSerie, setCurrentSerie] = useState(0);
-    const [info, setInfo] = useState("");
-
-    const onNextSerie = () => {
-        if(currentSerie === exercice.nbSeries) {
-            setInfo("Exercice terminé");
-            return;
-        }
-
-        setCurrentSerie(currentSerie + 1);
+  const onNextSerie = () => {
+    if (currentSerie === exercice.nbSeries) {
+      setInfo('Exercice terminé');
+      return;
     }
 
+    if (!isBreak) {
+      setIsBreak(true);
+      setCurrentSerie(currentSerie + 1);
+      return;
+    }
+  };
+
+  const modifyTime = (time: number) => {
+    if (currentTime + time < 0) {
+      setCurrentTime(0);
+      return;
+    }
+    setCurrentTime(currentTime + time);
+  };
+
+  React.useEffect(() => {
+    if (isBreak) {
+      const timerId = setInterval(() => {
+        setCurrentTime(currentTime - 1);
+        if (currentTime === 0) {
+          setCurrentTime(currentBreakTime);
+          setIsBreak(false);
+        }
+      }, 1000);
+      return () => clearInterval(timerId);
+    }
+  });
+
+  if (isBreak) {
     return (
-        <View style={styles.exerciceCard}>
-            {info ? <Text style={styles.info}>{info}</Text> : null}
-            <Text>{exercice.nom}</Text>
-            <Text>Nombre de séries : {exercice.nbSeries}</Text>
-
-            <View>
-                <Text>{currentSerie} sur {exercice.nbSeries} série{exercice.nbSeries > 1 ? 's' : null}</Text>
-            </View>
-
-            <Pressable style={styles.button} onPress={onNextSerie}>
-                <Text style={styles.text}>Prochaine série</Text>
-            </Pressable>
+      <View style={styles.exerciceCard}>
+        <Text>Temps de repos</Text>
+        <Text>{currentTime} secondes</Text>
+        <View
+          style={[
+            styles.btnWrapper,
+            {
+              flexDirection: 'row',
+            },
+          ]}
+        >
+          <View style={styles.btn}>
+            <Button
+              title="ADD 10s"
+              color="green"
+              onPress={async () => {
+                modifyTime(10);
+              }}
+            />
+          </View>
+          <View style={styles.btn}>
+            <Button
+              title="SUB 10s"
+              color="#c20e0e"
+              onPress={() => {
+                modifyTime(-10);
+              }}
+            />
+          </View>
         </View>
-    )
-}
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.exerciceCard}>
+        {info ? <Text style={styles.info}>{info}</Text> : null}
+        <Text>{exercice.nom}</Text>
+        <Text>Nombre de séries : {exercice.nbSeries}</Text>
+
+        <View>
+          <Text>
+            {currentSerie} sur {exercice.nbSeries} série{exercice.nbSeries > 1 ? 's' : null}
+          </Text>
+        </View>
+        <Pressable style={styles.button} onPress={onNextSerie}>
+          <Text style={styles.text}>Prochaine série</Text>
+        </Pressable>
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
-    exerciceCard : {
-        width: '90%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        backgroundColor: 'rgba(77, 77, 77, .7)'
-    },
-    button: {
-        width: '66%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 10,
-        borderRadius: 8,
-        elevation: 3,
-        backgroundColor: '#b0e600',
-    },
-    text: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'white',
-    },
-    info : {
-        width: '90%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        borderRadius: 8,
-        backgroundColor: '#ffd445',
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        marginTop: 10,
-        marginBottom: 10
-    }
-})
+  exerciceCard: {
+    color: '#fff',
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    backgroundColor: 'rgba(77, 77, 77, .7)',
+  },
+  button: {
+    width: '66%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    elevation: 3,
+    backgroundColor: '#b0e600',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  info: {
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderRadius: 8,
+    backgroundColor: '#ffd445',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  btnWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  btn: {
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+  },
+});
 
 export default ExerciceCard;
--- pages/runSeance.tsx
+++ pages/runSeance.tsx
@@ -1,69 +1,68 @@
-import React, {useState} from "react";
-import {Text, View, StyleSheet, ScrollView, TouchableOpacity} from "react-native";
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import CustomButton from '../components/button';
 
 // Import des composants et types
-import {Exercice} from '../types';
-import ExerciceCard from "../components/exerciceCard";
+import { Exercice } from '../types';
+import ExerciceCard from '../components/exerciceCard';
 
-const RunSeance = (props : any) => {
+const RunSeance = (props: any) => {
+  const [exercices, setCurrentExercices] = useState(props.seance?.exercices);
+  const [info, setInfo] = useState('');
+  const [currentExerciceKey, setCurrentExerciceKey] = useState(0);
 
-    const [exercices, setCurrentExercices] = useState(props.seance?.exercices);
-    const [info, setInfo] = useState("");
-    const [currentExerciceKey, setCurrentExerciceKey] = useState(0);
-
-    const next = () => {
-        if(currentExerciceKey >= Object.keys(exercices).length - 1) {
-            setInfo('Exercices terminés');
-            return;
-        }
-
-        setCurrentExerciceKey(currentExerciceKey + 1);
+  const next = () => {
+    if (currentExerciceKey >= Object.keys(exercices).length - 1) {
+      setInfo('Exercices terminés');
+      return;
     }
 
-    return (
-        <View style={{flex: 1}}>
-            <View style={{flex: 1}}>
-                {info ? <Text style={styles.info}>{info}</Text> : null}
+    setCurrentExerciceKey(currentExerciceKey + 1);
+  };
 
-                <View key={exercices[currentExerciceKey].id}>
-                    <ExerciceCard exercice={exercices[currentExerciceKey]} />
-                </View>
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        {info ? <Text style={styles.info}>{info}</Text> : null}
 
-                <View style={{marginTop : 15}}>
-                    <CustomButton
-                        title='Prochain exercice'
-                        onPress={() => {
-                            next();
-                        }}
-                    />
-                </View>
-            </View>
-
-            <View style={{height: 100}}>
-                <CustomButton
-                    title='Retour'
-                    onPress={() => {
-                        props.onClose();
-                    }}
-                />
-            </View>
+        <View key={exercices[currentExerciceKey].id}>
+          <ExerciceCard exercice={exercices[currentExerciceKey]} />
         </View>
-    )
-}
+
+        <View style={{ marginTop: 15 }}>
+          <CustomButton
+            title="Prochain exercice"
+            onPress={() => {
+              next();
+            }}
+          />
+        </View>
+      </View>
+
+      <View style={{ height: 100 }}>
+        <CustomButton
+          title="Retour"
+          onPress={() => {
+            props.onClose();
+          }}
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    info : {
-        width: '90%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        borderRadius: 8,
-        backgroundColor: '#ffd445',
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        marginTop: 10,
-        marginBottom: 10
-    }
-})
+  info: {
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderRadius: 8,
+    backgroundColor: '#ffd445',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
 
 export default RunSeance;
